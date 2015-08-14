1abd6ec2ce05cc2e6433ac25ca1ad612a92bc524
Michael Summers <student@epicodus.com>
2015-08-14 10:17:11 -0700

    Add spec, class, and related methods to create a definition, add a second definition, and output the result.

--- spec/Word_Definition_spec.rb
+++ spec/Word_Definition_spec.rb
@@ -1,5 +1,6 @@
 require('rspec')
 require('Word')
+require('Definition')
 
 describe ('Word') do
   before() do
@@ -27,6 +28,20 @@ describe ('Word') do
       test_word = Word.new({:word => "bobcat", :definition => "a small North American cat species with a barred and spotted coat and a short tail."})
       test_word.save_word()
       expect(Word.return_specific(0)).to(eq(test_word))
+    end
+  end
+end
+
+describe("Definition") do
+
+  describe(":definition") do
+    it("returns definitions") do
+      #word is experimentalism
+      new_definition_1 = "fondness for experimenting or innovating"
+      new_definition_2 = "doctrine or practice of relying on experimentation; empiricism"
+      test_definition = Definition.new(new_definition_1)
+      test_definition.add_definition(new_definition_2)
+      expect(test_definition.definition()).to(eq([new_definition_1, new_definition_2 ]))
     end
   end
 end

0067f169872b05b946fd49f769be9056141ad006
Michael Summers <student@epicodus.com>
2015-08-14 09:32:37 -0700

    Added spec and associated methods to make a Word object and return it

--- spec/Word_Definition_spec.rb
+++ spec/Word_Definition_spec.rb
@@ -9,4 +9,12 @@ describe ('Word') do
       expect(Word.all()).to(eq([test_word]))
     end
   end
+
+  describe(':id') do
+    it('creates a word and returns just its id number') do
+      test_word = Word.new({:word => "bobcat", :definition => "a small North American cat species with a barred and spotted coat and a short tail."})
+      test_word.save_word()
+      expect(Word.return_specific(1)).to(eq([test_word]))
+    end
+  end
 end
